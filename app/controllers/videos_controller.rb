5dc21c7364b1102387ec71c9e1b5fe789aa30a3d
kamitoshi <kroo.aq.74@gmail.com>
2020-07-13 10:55:54 +0900

    setting video controller

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,87 @@
+class VideosController < ApplicationController
+  before_action :set_video, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @videos = Video.all
+  end
+
+  def show
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = current_student.videos.build(video_params)
+    if @video.save
+      flash[:success] = "新しい動画を投稿しました"
+      redirect_to videos_path
+    else
+      flash.now[:danger] = "動画の投稿に失敗しました"
+      render :new
+    end
+  end
+
+  def request_new
+    @video = Video.new
+  end
+
+  def request_create
+    @video = current_student.videos.build(video_params)
+    @video.request_id = params[:request_id]
+    if @video.save
+      flash[:success] = "リクエストの回答動画を投稿しました"
+      redirect_to videos_path
+    else
+      flash.now[:danger] = "動画の投稿に失敗しました"
+      render :request_new
+    end
+  end
+
+  def edit
+    unless @video.student == current_student
+      flash[:danger] = "他の人がアップした動画は編集できません"
+      redirect_to videos_path
+    end
+  end
+
+  def update
+    if @video.student == current_student
+      if @video.update(video_params)
+        flash[:success] = "動画の内容を編集しました"
+        redirect_to video_path(@video)
+      else
+        flash.now[:danger] = "編集できませんでした"
+        render :edit
+      end
+    else
+      flash[:danger] = "他の人がアップした動画は編集できません"
+      redirect_to videos_path
+    end
+  end
+
+  def destroy
+    if @video.student == current_student
+      if @video.destroy
+        flash[:success] = "動画を削除しました"
+        redirect_to videos_path
+      else
+        flash[:danger] = "削除できませんでした"
+        redirect_to video_path(@video)
+      end
+    else
+      flash[:danger] = "他の人のアップした動画は削除できません"
+      redirect_to videos_path
+    end
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:student_id, :request_id, :academic_stage, :subject, :subject_content, :title, :content, :detail, :count)
+  end
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/new'
+  get 'videos/edit'
   root 'home#top'
   get 'home/about'
   devise_for :students, controllers:{
@@ -7,7 +11,10 @@ Rails.application.routes.draw do
     registrations: "students/registrations"
   }
   resources :students, only:[:index, :show, :edit, :update, :destroy]
-  resources :requests
+  resources :requests do
+    get "videos/request_new"
+    post "videos/request_create"
+  end
   resources :videos
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
